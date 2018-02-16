be7fcacd316cca7e5475d84afa8dcd803df9dfd6
lalmeida89 <lalmeida89@gmail.com>
2018-02-16 17:32:24 -0500

    blah

---
+++ index.js
@@ -0,0 +1,61 @@
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(Date.parse(new Date("Mar 10, 2018 20:01:00")));
+initializeClock('ccl', deadline);
+
+var now = new Date().getTime();
+
+var distance = deadline - now;
+
+ if (distance < 0) {
+  document.getElementById("ccl").style.display = "none";
+  document.getElementById("finish").style.display = "block";
+}
+
+$('.gifBtn').click(function(e){
+  $('.modal').removeClass('hidden');
+  $('.gifBtn').addClass('hidden');
+  $('.body').addClass('hidden');
+});
+
+$('.close').click(function(e) {
+    $('.modal').addClass('hidden');//close the modal when clicking on the x
+    $('.gifBtn').removeClass('hidden');
+});
